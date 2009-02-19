75f8092047400f6ae3a55ce6e1fbd41de78b9dc8
root <3712 Echoway Rd, Richmond VA 23234>
2009-02-19 12:16:30 -0600

    can search inventories by rojo or verde

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -18,10 +18,24 @@ class Inventory < ActiveRecord::Base
 	belongs_to :entity
 	belongs_to :product
 	def self.search(search, page)
-		paginate :per_page => 20, :page => page,
-			       :conditions => ['(products.name like :search OR products.upc like :search) AND (entities.id=:current_location) AND (products.product_type_id=1)', {:search => "%#{search}%", :current_location => "#{User.current_user.location_id}"}],
-			       :order => 'inventories.created_at DESC',
-			       :joins => 'inner join products on products.id = inventories.product_id inner join entities on entities.id = inventories.entity_id'
+		if search[0..3].downcase=='rojo'
+			search = search[4..search.length].downcase.strip
+			paginate :per_page => 20, :page => page,
+				:conditions => ['(products.name like :search OR products.upc like :search) AND (entities.id=:current_location) AND (products.product_type_id=1) AND (inventories.quantity<inventories.min) AND (inventories.to_order=0 OR inventories.to_order is null)', {:search => "%#{search}%", :current_location => "#{User.current_user.location_id}"}],
+				:order => 'inventories.created_at DESC',
+				:joins => 'inner join products on products.id = inventories.product_id inner join entities on entities.id = inventories.entity_id'
+		elsif search[0..4].downcase=='verde'
+			search = search[5..search.length].downcase.strip
+			paginate :per_page => 20, :page => page,
+				:conditions => ['(products.name like :search OR products.upc like :search) AND (entities.id=:current_location) AND (products.product_type_id=1) AND (inventories.to_order > 0)', {:search => "%#{search}%", :current_location => "#{User.current_user.location_id}"}],
+				:order => 'inventories.created_at DESC',
+				:joins => 'inner join products on products.id = inventories.product_id inner join entities on entities.id = inventories.entity_id'
+		else	       
+			paginate :per_page => 20, :page => page,
+				:conditions => ['(products.name like :search OR products.upc like :search) AND (entities.id=:current_location) AND (products.product_type_id=1)', {:search => "%#{search}%", :current_location => "#{User.current_user.location_id}"}],
+				:order => 'inventories.created_at DESC',
+				:joins => 'inner join products on products.id = inventories.product_id inner join entities on entities.id = inventories.entity_id'
+		end
 	end
 	def self.search_wo_pagination(search, page)
 		find     :conditions => ['(products.name like :search OR products.upc like :search) AND (entities.id=:current_location) AND (products.product_type_id=1)', {:search => "%#{search}%", :current_location => "#{User.current_user.location_id}"}],
