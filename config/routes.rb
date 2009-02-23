e3fa04ed86b19afebc36db2b02552941c7f4de2f
root <3712 Echoway Rd, Richmond VA 23234>
2009-02-23 16:53:34 -0600

    fixed logout issue

--- config/routes.rb
+++ config/routes.rb
@@ -69,9 +69,9 @@ ActionController::Routing::Routes.draw do |map|
 
 	map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
 	map.signup '/signup', :controller => 'users', :action => 'new'
-	map.login '/login', :controller => 'session', :action => 'new'
-	map.logout '/logout', :controller => 'session', :action => 'destroy'
-	map.reject '/reject', :controller => 'session', :action => 'reject'
+	map.login '/login', :controller => 'sessions', :action => 'new'
+	map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+	map.reject '/reject', :controller => 'sessions', :action => 'reject'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
