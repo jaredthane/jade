393b6c1d0f590e678ea347ef13435169c538e7ec
root <3712 Echoway Rd, Richmond VA 23234>
2009-03-04 14:23:28 -0600

    added calcualtions to new payments

--- public/javascripts/payment_edit.js
+++ public/javascripts/payment_edit.js
@@ -1,3 +1,12 @@
 function calculate_total() {
-	$("total_paid").value=$("presented_field").value-$("returned_field").value
+	$("total_paid").value=$("presented_field").value-$("returned_field").value;
+	$("debt_field").value=$("total_price").value-$("presented_field").value+$("returned_field").value;
+	if ($("presented_field").value-$("total_price").value < 0){
+		$("change_div").hide();
+		$("debt_div").show();
+	} else {
+		$("change_div").show();
+		$("debt_div").hide();
+	}
+	$("recommended_change").value=$("presented_field").value-$("total_price").value;
 }
