aeaa3930f82492440effd0853b2d2aed1b05ae63
root <3712 Echoway Rd, Richmond VA 23234>
2009-03-24 00:22:49 -0600

    made rojo filter really work

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -28,11 +28,12 @@ class Inventory < ActiveRecord::Base
 	def self.search(search, page)
 		search = (search ||"")
 		if search[0..3].downcase=='rojo'
+			joins = 'inner join products on products.id = inventories.product_id inner join entities on entities.id = inventories.entity_id left join (SELECT `lines`.product_id, sum(`lines`.quantity) as quantity FROM `lines` left join orders on orders.id=lines.order_id WHERE orders.order_type_id=1 AND orders.vendor_id=' + User.current_user.location_id.to_s + ' AND lines.received is null GROUP BY `lines`.product_id) as sales on sales.product_id = products.id left join (SELECT `lines`.product_id, sum(`lines`.quantity) as quantity FROM `lines` left join orders on orders.id=lines.order_id WHERE orders.order_type_id=2  AND orders.client_id=' + User.current_user.location_id.to_s + ' AND lines.received is null GROUP BY `lines`.product_id) as purchases on purchases.product_id = products.id'
 			search = search[4..search.length].downcase.strip
 			paginate :per_page => 20, :page => page,                                                                                                                     
-				:conditions => ['(products.name like :search OR products.upc like :search) AND (entities.id=:current_location) AND (products.product_type_id=1) AND (inventories.quantity + purchases.quantity < sales.quantity + inventories.min) AND (inventories.to_order=0 OR inventories.to_order is null)', {:search => "%#{search}%", :current_location => "#{User.current_user.location_id}"}],
+				:conditions => ['(products.name like :search OR products.upc like :search) AND (entities.id=:current_location) AND (products.product_type_id=1) AND (COALESCE(inventories.quantity,0) + COALESCE(purchases.quantity,0) < COALESCE(sales.quantity,0) + COALESCE(inventories.min,0)) AND (inventories.to_order=0 OR inventories.to_order is null)', {:search => "%#{search}%", :current_location => "#{User.current_user.location_id}"}],
 				:order => 'inventories.created_at DESC',
-				:joins => 'inner join products on products.id = inventories.product_id inner join entities on entities.id = inventories.entity_id left join (SELECT `lines`.product_id, sum(`lines`.quantity) as quantity FROM `lines` left join orders on orders.id=lines.order_id WHERE orders.order_type_id=1 AND lines.received is null GROUP BY `lines`.product_id) as sales on sales.product_id = products.id left join (SELECT `lines`.product_id, sum(`lines`.quantity) as quantity FROM `lines` left join orders on orders.id=lines.order_id WHERE orders.order_type_id=2 AND lines.received is null GROUP BY `lines`.product_id) as purchases on purchases.product_id = products.id'
+				:joins => joins
 		elsif search[0..4].downcase=='verde'
 			search = search[5..search.length].downcase.strip
 			paginate :per_page => 20, :page => page,
