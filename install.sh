8b731f4b3347456450d9774985cd19caf1ea3bd1
jared <jared@jared-desktop.(none)>
2009-04-29 23:05:40 -0600

    added clean.sql and added commands to install script

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
 #Install ruby
+echo "Installing Ruby"
 sudo aptitude install ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8 libreadline-ruby1.8 libruby1.8 libopenssl-ruby sqlite3 libsqlite3-ruby1.8
 
 # Install Ruby Gems
+echo "Installing Ruby Gems"
 wget http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz
 tar -xf rubygems-1.3.1.tgz
 cd rubygems-1.3.1
 sudo ruby setup.rb
+cd ..
 
-# Install Rails
-sudo gem install rails
 
+echo "Installing MySQL"
 sudo aptitude install mysql libmysqlclient-dev
 
+# Install Rails
+echo "Installing Rails"
+sudo gem install rails
+echo "Installing Prawn"
 sudo gem install prawn
+echo "Installing Pagination"
 sudo gem install will_paginate
-
-# install ruby mysql gem
+echo "Installing MySQL Gem"
 sudo gem install mysql
-
-echo "You still need to configure mysql and add the database"
-
+echo "Installing Jade database"
+sudo mysql -p < clean.sql
