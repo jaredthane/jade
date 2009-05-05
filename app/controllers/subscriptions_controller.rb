388c2788276648029b6d71a7d08a62681662cd7d
jared <jared@jared-desktop.(none)>
2009-05-04 23:11:28 -0600

    created subscription views

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,100 @@
+# Jade Inventory Control System
+#Copyright (C) 2009  Jared T. Martin
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied subscription of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class SubscriptionsController < ApplicationController
+  # GET /subscriptions
+  # GET /subscriptions.xml
+  def index
+		@subscriptions = Subscription.search(params[:search], params[:page])
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @subscriptions }
+    end
+  end
+
+  # GET /subscriptions/1
+  # GET /subscriptions/1.xml
+  def show
+    @subscription = Subscription.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @subscription }
+    end
+  end
+
+  # GET /subscriptions/new
+  # GET /subscriptions/new.xml
+  def new
+    @subscription = Subscription.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @subscription }
+    end
+  end
+
+  # GET /subscriptions/1/edit
+  def edit
+    @subscription = Subscription.find(params[:id])
+  end
+
+  # POST /subscriptions
+  # POST /subscriptions.xml
+  def create
+    @subscription = Subscription.new(params[:subscription])
+
+    respond_to do |format|
+      if @subscription.save
+        flash[:notice] = 'Garantía ha sido creado exitosamente.'
+        format.html { redirect_to(subscriptions_url) }
+        format.xml  { render :xml => @subscriptions, :status => :created, :location => @subscription }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @subscription.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /subscriptions/1
+  # PUT /subscriptions/1.xml
+  def update
+    @subscription = Subscription.find(params[:id])
+
+    respond_to do |format|
+      if @subscription.update_attributes(params[:subscription])
+        flash[:notice] = 'Garantía ha sido actualizado exitosamente.'
+        format.html { render :action => "index" }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @subscription.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /subscriptions/1
+  # DELETE /subscriptions/1.xml
+  def destroy
+    @subscription = Subscription.find(params[:id])
+    @subscription.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(subscriptions_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
