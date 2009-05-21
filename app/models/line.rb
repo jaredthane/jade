4d469dd14ded8b241796e9fbe23936644c330ddf
root <3712 Echoway Rd, Richmond VA 23234>
2009-05-20 19:04:13 -0600

    tested and confirmed every step of revenue_accounts

--- app/models/line.rb
+++ app/models/line.rb
@@ -106,22 +106,28 @@ class Line < ActiveRecord::Base
 		for pref in Preference.find(:all, :order=>'value')
 			case pref.id
 			when 1 #Product
+				logger.debug " Grabbing revenue account from Product"
 				return product.revenue_account if product.revenue_account
 			when 2 #Category
+				logger.debug " Grabbing revenue account from Category"
 				if product.product_category
 					return product.product_category.revenue_account if product.product_category.revenue_account
 				end
 			when 3 #Vendor
+				logger.debug " Grabbing revenue account from Vendor"
 				if product.vendor
 					return product.vendor.revenue_account if product.vendor.revenue_account
 				end
 			when 4 #Site
+				logger.debug " Grabbing revenue account from Site"
 				if order.vendor
 					return order.vendor.revenue_account if order.vendor.revenue_account
 				end
 			when 5 #Current User
+				logger.debug " Grabbing revenue account from Current User"
 				return User.current_user.firm_account if User.current_user.firm_account
 			when 6 #Clients Rep
+				logger.debug " Grabbing revenue account from Clients Rep"
 				if order.client
 					return order.client.user.firm_account if order.client.user.firm_account
 				end
