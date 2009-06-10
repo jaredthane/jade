3f4dd2ab69500ceae98e435e3e878af7e772f5d6
root <3712 Echoway Rd, Richmond VA 23234>
2009-06-10 12:58:30 -0600

    fixed link to clients path

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -87,7 +87,7 @@ class EntitiesController < ApplicationController
   		client.process_subscriptions()
   	end
     respond_to do |format|
-      format.html { redirect_to(clients_path(:search=>params[:search], :filter=>params[:filter])) }
+      format.html { redirect_to(clients_path) }
       format.xml  { render :xml => @subscription }
     end
   end
