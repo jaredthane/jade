9724420c6c5a81c0867ccc9681ab96e112da4702
Jared T Martin <3712 Echoway Rd, Richmond VA 23234>
2009-08-26 18:44:27 -0600

    Added generic product views

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,17 @@
+from jade.inventory.models import Product
+from jade.inventory.forms import ProductForm
+from jade.common.views import *
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def show_product(request, object_id):
-	pass
+	return generic_show(request, object_id, Product)
+	
+@login_required
 def edit_product(request, object_id=None):
-	pass
+	return generic_edit(request, Product, ProductForm, object_id)
+	
+@login_required
 def index_products(request):
-	pass
+	object_list =	Product.objects.all()
+	return generic_index(request, Product, object_list)
