8904edb5dd62ee9496d84775689d31e97de52f8a
Jared T Martin <3712 Echoway Rd, Richmond VA 23234>
2009-11-03 10:20:12 -0600

    Added backup script

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+TIMESTAMP=`date +%m%d%Y`
+# edit the following 6 lines
+BACKUP_FOLDER=/srv/jade/backup
+MYSQL_USER=root
+MYSQL_PASSWORD=YOURMYSQLPASSWORD
+DBNAME=Jade
+MYSQL_HOST=localhost
+
+# don't edit this line
+/usr/bin/mysqldump -h$MYSQL_HOST -q -u$MYSQL_USER -p$MYSQL_PASSWORD $DBNAME | gzip > $BACKUP_FOLDER/$DBNAME-$TIMESTAMP.sql.gz 
+
+# Use this cron tab entry:
+# 5 1 * * * /srv/jade/backup.sh 
