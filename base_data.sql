d22cb568f48dac53781a544b6b95c3a373fd853e
Jared T Martin <3712 Echoway Rd, Richmond VA 23234>
2009-12-23 21:52:38 -0600

    Upgraded install script

--- base_data.sql
+++ base_data.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `accounts`
 --
-
+create database Jade;
+use Jade;
 DROP TABLE IF EXISTS `accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
