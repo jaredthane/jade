342813d9252b602f3097a7ddeab57634710e2642
Jared T Martin <3712 Echoway Rd, Richmond VA 23234>
2009-12-30 13:24:34 -0600

    add payment works smoothly and was simplified

--- public/javascripts/payment_edit.js
+++ public/javascripts/payment_edit.js
@@ -1,7 +1,12 @@
+function calculate_change() {
+	change = (parseFloat($("#presented_field").val())-parseFloat($("#total_price").val()) + parseFloat($("#other_payments").val())).toFixed(2)
+	if (change < 0) {
+	$("#returned_field").val(0);
+	}else {
+	$("#returned_field").val(change);
+	}
+	calculate_total();
+}
 function calculate_total() {
 	$("#total_paid").val((parseFloat($("#presented_field").val()) - parseFloat($("#returned_field").val())).toFixed(2));
-	$("#returned_field").val((parseFloat($("#presented_field").val())-parseFloat($("#total_price").val()) + parseFloat($("#other_payments").val())).toFixed(2));
 }
-
-
-
