7297442c03bca5519c16671c03cf863bf2177ba7
Jared Martin <3712 Echoway Rd, Richmond VA 23234>
2010-03-24 11:44:39 -0600

    allow for null barcode filename

--- db/migrate/20100304170341_add_total_revenue_and_total_cost.rb
+++ db/migrate/20100304170341_add_total_revenue_and_total_cost.rb
@@ -1,13 +1,5 @@
 class AddTotalRevenueAndTotalCost < ActiveRecord::Migration
   def self.up
-    for line in Line.all()
-      if !line.cost
-        line.cost = line.product.cost(line.order.client) if line.order_type_id == Order::COUNT
-        line.cost = line.product.cost(line.order.client) if line.order_type_id == Order::PURCHASE
-        line.cost = line.product.cost(line.order.vendor) if line.order_type_id == Order::SALE        
-        line.send(:update_without_callbacks)
-      end
-    end
     add_column :orders, :total_revenue, :decimal, :precision => 8, :scale => 2
     add_column :orders, :total_expense, :decimal, :precision => 8, :scale => 2
     for order in Order.all()
